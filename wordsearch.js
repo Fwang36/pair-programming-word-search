9265a5e6ba17352acd2cf3b9939d292974d7d236
Francis <fwang36@gmail.com>
2022-02-16 19:33:27 -0500

    fixed word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+    let joined = "";
+    let vertJoin = "";
+    let vertical = [];
+    
+    for (let i = 0; i < letters[0].length; i++) {
+        for (let j = 0; j < letters.length; j++) {
+            vertical.push(letters[j][i]);
+        }
+        vertJoin = vertical.join('');
+        if (vertJoin.includes(word)) {
+            return true;
+        } else vertMatch = false;
+
+        vertical = [];
+    }
+    for (let k = 0; k < letters.length; k++) {
+
+        joined = letters[k].join("");
+        if (joined.includes(word)) {
+            return true;
+
+        } else horiMatch = false;
+
+
+    }
+    if (vertMatch === false && horiMatch === false) {
+        return false;
+    }
+};
+
+module.exports = wordSearch;
